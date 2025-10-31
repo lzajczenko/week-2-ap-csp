a7c2d30b3eef0c915d48f99b1de44a34d5899cf7
lzajczenko <lzajczenko@cps.edu>
2025-10-31 18:26:46 +0000

    final_challenge

--- 4_practice_prompts.py
+++ 4_practice_prompts.py
@@ -19,17 +19,32 @@ print(500 + 55)
 
 ##############################################################################################################
 # Find 3 objects around the room and create variables from it,
-# Insert those variables into an f-string sentence(look at slide 22)in repl.it
+clock = "11am"
+computer = "48 computers"
+phones= "30 phone slots"
 
+# Insert those variables into an f-string sentence(look at slide 22)in repl.it
+print(f"The time is {clock}, there are {phones}, and {computer}")
 
 # Familiarize yourself with the syntax of the print() function.
 # Print your name.
+name = "Lily"
+print(name)
+
 # Print today's date.
+date = "October 31"
+print(date)
 # Print the name of your favorite movie.
-
+movie = "How the Grinch stole Christmas"
+print (movie)
 # Print your name and age on separate lines using a single print() function.
+name = "Lily"
+age = 17
+print(f"{name} \n {age}")
 # Use f-strings to print a message like: "In 10 years, [Your Name] will be [Your Age + 10] years old."
-
+age = 17
+new_age = age + 10
+print (f"In 10 years, {name} will be, {new_age} years old") 
 ##############################################################################################################
 
 ###########################String Practice##################################
@@ -46,6 +61,14 @@ print(500 + 55)
 #challenge
 # find a summary of the movie blue beetle online and create a 
 # variable called blue_beetle_summary and print it it out to the screen
+
+beetlesummary = "In Antarctica, members of Kord Industries, led by its co-founder and CEO Victoria Kord, locate an ancient alien artifact known as the Scarab. Meanwhile, Jaime Reyes returns to his hometown of Palmera City after graduating from Gotham Law University, only to learn that his family is facing eviction from their home due to financial difficulties. Jaime's sister Milagro manages to get him a job at Victoria's mansion. However, both are fired after Jaime stops a confrontation between Victoria and her niece Jenny. Jenny later tells Jaime to meet her at Kord Tower the next day to discuss a job opportunity. The next day, Jenny finds that Victoria is using the Scarab for her One Man Army Corps (OMAC) projects. She steals the Scarab and avoids security by giving it to Jaime, hidden inside a Big Belly Burger to-go box. At home, Jaime's family convinces him to open the box, and when Jaime ends up holding the Scarab, it activates and fuses with him, encasing him in an exoskeleton. Jaime then gets launched from the stratosphere to all over the city. Jaime tracks down Jenny for answers, rescuing her from Victoria's armed forces. She tells Jaime the Scarab is a sentient weapon and it has chosen Jaime to be its host. With the help of Jaime's uncle Rudy, Jaime and Jenny break into Kord Tower to retrieve a smartwatch that once belonged to Jenny's father Ted, but are attacked by Victoria's bodyguard Ignacio Carapax, who has an OMAC prototype infused in his body. The Scarab, revealed to be named Khaji-Da, temporarily takes over Jaime's body and battles Carapax, but Jaime refuses to let it kill Carapax. Rudy and Jenny help incapacitate Carapax, then escape with Jaime to Jenny's childhood home. Jenny uses Ted's watch to activate his secret laboratory and reveals that Ted was originally a vigilante named Blue Beetle who spent his life studying Khaji-Da before mysteriously disappearing, leaving his company in Victoria's hands. When they notice Victoria's helicopter flying toward Jaime's home, Jaime summons Khaji-Da and returns to protect his family, but Jaime's father Alberto collapses and dies from a heart attack, distracting Jaime and allowing Carapax to capture him. Jaime is taken to an island fortress near Cuba, where he is strapped to a machine that downloads information from Khaji-Da into the OMACs. While unconscious, Jaime experiences a vision from Alberto in the afterlife, who encourages him to embrace his destiny as the new Blue Beetle. While Jenny and the Reyes family use Ted's Bugship and its weapons arsenal to storm the island, Jaime awakens and escapes as Carapax's OMAC suit evolves into a more powerful form. Jaime reunites with his family, then encounters Carapax and battles him. Jaime nearly kills Carapax but is stopped by Khaji-Da, who shows him Carapax's memories it obtained through the information download, from which Jaime learns of Carapax's tragic past, including his enslavement by Victoria for the OMAC experiments and the death of his mother at her hands. Moved by this revelation, Jaime spares Carapax, who is prompted to turn on Victoria and sets his OMAC suit to explode and destroy the island, himself, and Victoria, as vengeance for his mother. As the Reyes family escapes the island, they take time to mourn Alberto. In the aftermath, Jenny becomes the new CEO of Kord Industries and promises to repair the damage caused to the Reyes family by helping them rebuild their home. As the neighbors gather around the remains of the Reyes family's home and provide support, Jaime kisses Jenny and then offers to fly her to the Kord Estate. In a mid-credits scene, a distorted recording is broadcast in Ted's laboratory, attempting to inform Jenny he is alive."
+print(len(beetlesummary))
+print(beetlesummary.upper())
+print(beetlesummary)
+print(beetlesummary.lower())
+print(beetlesummary[-7:])
+print(beetlesummary[::-1])
 
 # print the length of the summary
 # upper case the entire summary
@@ -66,26 +89,27 @@ print(500 + 55)
 # Write Python code that allows the user to enter their answer, by making them the following question:
 # What are you learning today?
 # Your code must be able to print to the screen whatever is entered by the user (use the print function).
+learn = input("What are you learning today?: ")
+print(f"You are learning {learn} today.")
 
 # Input Practice #2
 # Write Python code that allows the user to enter their answer, by making them the following question:
 # Where are you from?
 # Your code must be able to print to the screen whatever is entered by the user (use the print function).
+place = input("Where are you from?: ")
+print(f"You are from {place}.")
 
 # Input Practice #3
 # Write Python code that displays the user's full name on the screen, by allowing them to enter their first and last name with the following instructions:
 # What is your name?
 # What is your surname?
 # The code must be able to print the user's first and last name on the screen, separated by a space.
+name = input("What is your name?: ")
+surname = input("What is your surname?: ")
+print(f"Your full name is {name} {surname}.")
 
 # Exercise:
 # Write a program that asks the user for their name and favorite color, then prints a message using both pieces of information.
-
-
-
-
-
-
-
-
-
+name2 = input("What is your name?: ")
+color = input("What is your favorite color?: ")
+print(f"Your name is {name} and your favorite color is {color}.")
--- 5_finalChallenge.py
+++ 5_finalChallenge.py
@@ -8,7 +8,7 @@
 
 # I know that in the real world, we wouldn’t need to develop a software just to ask it two questions. But until we learn a bit more functionality, well, our programs are going to have to stay in the realm of simplicity. Yet if you’re just starting out, this is going to be quite a challenge.
 
-# You’re going to create Python code that asks your friend to answer two questions that require a single word each, and then displays those combined words on the screen to form a creative brand.
+# You’re going to create Python code that asks your friend to answer two questions that require a single word each, and then displays those combined words on the screen to form a creative brand. !!!!!!!!!!!!!!!!!!!!!!!!!
 
 # You can use any questions you want. The idea is for the outcome to be original, creative, and even funny. And if you want to add some difficulty to your challenge, I suggest you try to have the name of the beer printed in quotation marks on the screen.
 
